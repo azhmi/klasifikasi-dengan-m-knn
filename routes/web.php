5ae13b603252f1047e36f8688f49e8d5c20e03ca
Fauzal azhmi <fauzal.azhmi@students.uin-suska.ac.id>
2020-01-28 20:54:11 +0700

    add template

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/beranda', function () {
+    return view('layout.index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
